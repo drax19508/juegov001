7e96356891c8dbbea8b7d9b562cde3f614c5f1d6
drax19508 <drax19508@gmail.com>
2025-08-31 18:29:14 -0500

    Actualizar pxt.json, main.blocks, main.ts, images.g.jres, images.g.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,14 @@
-let mySprite = sprites.create(assets.image`Estudiante`, SpriteKind.Player)
+controller.right.onEvent(ControllerButtonEvent.Pressed, function () {
+    // Velocidad de la animación (ms por frame)
+    // Se repite en bucle
+    animation.runImageAnimation(
+    mySprite,
+    assets.animation`myAnim`,
+    100,
+    true
+    )
+})
+let mySprite: Sprite = null
+mySprite = sprites.create(assets.image`Estudiante`, SpriteKind.Player)
 controller.moveSprite(mySprite)
 mySprite.setStayInScreen(true)
