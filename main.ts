fb26b75bb6024b67f578ead044a8105294c96bfc
camilo26089 <camilo26089@gmail.com>
2025-09-02 13:20:29 -0500

    Actualizar main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -12,3 +12,6 @@ let mySprite: Sprite = null
 mySprite = sprites.create(assets.image`Estudiante`, SpriteKind.Player)
 controller.moveSprite(mySprite)
 mySprite.setStayInScreen(true)
+forever(function () {
+    music.play(music.stringPlayable("G A E E G E E D ", 120), music.PlaybackMode.UntilDone)
+})
