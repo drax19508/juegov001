1ba42c609f2399c751ef411cc8a4c46fcaa0acd0
drax19508 <drax19508@gmail.com>
2025-09-12 19:50:15 -0500

    Actualizar pxt.json, main.blocks, main.ts, images.g.jres, images.g.ts

--- main.ts
+++ main.ts
@@ -4,12 +4,12 @@ controller.right.onEvent(ControllerButtonEvent.Pressed, function () {
     animation.runImageAnimation(
     mySprite,
     assets.animation`myAnim`,
-    100,
+    200,
     true
     )
 })
 let mySprite: Sprite = null
-mySprite = sprites.create(assets.image`Estudiante`, SpriteKind.Player)
+mySprite = sprites.create(assets.image`Estudiante2`, SpriteKind.Player)
 controller.moveSprite(mySprite)
 mySprite.setStayInScreen(true)
 forever(function () {
